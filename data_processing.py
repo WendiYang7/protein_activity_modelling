292e70bfb4a34e54cd9770e65bed02378dc5fa9c
Hong <hong.cao0824@gmail.com>
2024-05-23 16:18:22 +0800

    found ERBB1 and attempting to organize it

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,20 @@
+# %%
+# imports
 import pandas as pd
-from chembl_webresource_client import new_client
+from chembl_webresource_client.new_client import new_client
 
+# %%
+# target search for ERBB
 target = new_client.target
 target_query = target.search('ERBB')
 targets = pd.DataFrame.from_dict(target_query)
-print(targets)
+# print(targets)
+selected_target = targets.target_chembl_id[0]
+# print(selected_target)
+
+# %%
+# activity = new_client.activity
+# res = activity.filter(target_chembl_id=selected_target).filter(standard_type="IC50")
+# df = pd.DataFrame.from_dict(res)
+# print(df.head(3))
+# %%
