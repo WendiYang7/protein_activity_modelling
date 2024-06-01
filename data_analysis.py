a9b5fa082e2be60cf1a8ebf7e6a907ab741dbacc
Wendi Yang <wendiyang@WendideMacBook-Pro-2.local>
2024-06-01 18:59:06 +0800

    combined files and added functions

--- data_analysis.py
+++ data_analysis.py
@@ -1,18 +1,20 @@
 # %%
 import pandas as pd
-df = pd.read_csv('bioactivity_preprocessed_data.csv')
-# %%
 import numpy as np
 from rdkit import Chem
 from rdkit.Chem import Descriptors, Lipinski
+
 # %%
+
+df = pd.read_csv('bioactivity_preprocessed_data.csv')
+
 def lipinski(smiles, verbose = False):
     moldata = []
     for elem in smiles:
         mol =  Chem.MolFromSmiles(elem)
         moldata.append(mol)
 
-    baseData = np.arrange(1,1)
+    baseData = np.arange(1,1)
     i = 0
     for mol in moldata:
         desc_MolWt = Descriptors.MolWt(mol)
@@ -34,10 +36,22 @@ def lipinski(smiles, verbose = False):
     descriptors = pd.DataFrame(data=baseData, columns = columnNames)
 
     return descriptors
+# %%
+df_lipinski = lipinski(df.canonical_smiles)
+df_combined = pd.concat([df,df_lipinski], axis = 1)
 
+def AC50(input):
+    pAC50 = []
+
+    for i in input['standard_value_norm']:
+        molar = i*10**-9
+        pAC50.append(-np.log10(molar))
+
+        input['pAC50'] = pAC50
+        x = input.drop('standard_value_norm', 1)
+    
+        return x
 
 
     
-
-
-    
+# %%
